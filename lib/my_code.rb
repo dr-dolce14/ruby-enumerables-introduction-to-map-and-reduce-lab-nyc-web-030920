6913b4d6b6d4b61777282a5ff0aed5464d8ae1c4
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-10 19:38:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  i = 0
+  i = starting_point
   total = 0
   while i < source_array.length do
 total = total + source_array[i]
