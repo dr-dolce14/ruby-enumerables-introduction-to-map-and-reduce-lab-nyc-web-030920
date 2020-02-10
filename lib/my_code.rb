b16dca944ce8d8771ffb37d49bb7d983f19e2420
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-10 19:15:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,13 @@ while i < source_array.length do
 end
 result
 end
+
+def map_to_double(source_array)
+  result = []
+  i = 0
+  while i < source_array.length do
+    result << source_array[i] * 2
+    i +=1
+  end
+  result
+end
